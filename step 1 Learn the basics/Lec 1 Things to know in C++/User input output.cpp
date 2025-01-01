cc29b11eda182480456a8926f1a0e6917b9bbd2f
codedbykav <kavishakulshreshtha24@gmail.com>
2025-01-01 22:42:19 +0530

    Search Query Auto Complete

---
+++ step 1 Learn the basics/Lec 1 Things to know in C++/User input output.cpp
@@ -0,0 +1,5 @@
+// so the first question is a lil bit scam according to the sheet, as this question is harder than hard 
+
+// the question is on gfg ==== " Search Query Auto Complete 
+
+// LET IT BE AS IT IS !!
